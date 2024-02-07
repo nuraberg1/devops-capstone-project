94cdd22e5d7a790bd3df85c30fd8c3457dff3f33
Your Name <you@example.com>
2024-02-07 04:32:00 -0500

    added nose task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -24,6 +24,8 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -34,7 +36,10 @@ class TestAccountService(TestCase):
         app.config["DEBUG"] = False
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
+        
+        
         init_db(app)
+        
         talisman.force_https = False
     @classmethod
     def tearDownClass(cls):
